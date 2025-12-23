1cdd3d68279886a64624fdbaa307fa35c2516458
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-12-23 11:57:22 -0300

    feat: new brand list button to brand create page and brand create form styled with bt5

--- brands/forms.py
+++ brands/forms.py
@@ -6,3 +6,13 @@ class BrandForm(forms.ModelForm):
     class Meta:
         model = models.Brand
         fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 5}
+            ),
+        }
+        labels = {
+            'name': 'Nome',
+            'description': 'Descrição',
+        }
